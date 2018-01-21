f01ad7204424c84cf76376dba85b63d12bf535b8
Georgi N. Kostov <joromir@protonmail.com>
2018-01-21 14:17:10 +0200

    define factorial

--- src/hello_world.go
+++ src/hello_world.go
@@ -10,9 +10,23 @@ func my_sum(operand_one int, operant_two int) int {
     return operand_one + operant_two
 }
 
+func factorial(number int) int {
+  if number == 1 {
+    return 1
+  } else {
+    return number * factorial(number - 1)
+  }
+}
+
 func main() {
     hi()
 
     fmt.Print(my_sum(12, 12))
+    fmt.Printf("\n\n\n")
+
+    fmt.Print(factorial(5))
+    fmt.Printf("\n\n\n")
+
+    fmt.Print(factorial(10))
     fmt.Printf("\n\n\n")
 }
